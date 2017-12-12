9df32a7ee9f4c2ff0fd6a3e91eb2d512fd766667
Nick Fox-Gieg <nick@fox-gieg.com>
2017-12-12 11:35:38 -0500

    cleanup

--- install.bat
+++ install.bat
@@ -5,4 +5,5 @@ cd %cd%
 
 copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.77\scripts\addons"
 copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.78\scripts\addons"
+copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.79\scripts\addons"
 @pause
