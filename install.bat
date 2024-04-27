7d8f140e3c28631696c909fc05500d0318f53749
Nick Fox-Gieg <nick@fox-gieg.com>
2024-04-27 14:55:36 -0400

    move to init.py

--- install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set BUILD_TARGET=freestyle_to_gpencil.py
-cd %cd%
- 
-copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.77\scripts\addons"
-copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.78\scripts\addons"
-copy %BUILD_TARGET% "%homepath%\AppData\Roaming\Blender Foundation\Blender\2.79\scripts\addons"
-@pause
